d2a233baa90406d5f43ee5f5b075a84fb8f670c8
Uwe Trottmann <uwe+git@uwetrottmann.com>
2017-11-10 09:34:38 +0100

    Update extension example for API 2.0.0.
    - Use a BroadcastReceiver to register extension.
    - Register ExampleExtension as a job service (for O+).

---
+++ app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/ExampleExtensionReceiver.java
@@ -0,0 +1,17 @@
+package com.uwetrottmann.seriesguide.extensionexample.app;
+
+import com.battlelancer.seriesguide.api.SeriesGuideExtension;
+import com.battlelancer.seriesguide.api.SeriesGuideExtensionReceiver;
+
+
+public class ExampleExtensionReceiver extends SeriesGuideExtensionReceiver {
+    @Override
+    protected int getJobId() {
+        return 1;
+    }
+
+    @Override
+    protected Class<? extends SeriesGuideExtension> getExtensionClass() {
+        return ExampleExtension.class;
+    }
+}
