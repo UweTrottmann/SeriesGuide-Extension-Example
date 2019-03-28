7c6277705150506baa18dce26c1201448fddf128
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-03-28 11:22:24 +0100

    Use Java 8 language features.

--- app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/MainActivity.java
+++ app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/MainActivity.java
@@ -3,7 +3,7 @@ package com.uwetrottmann.seriesguide.extensionexample.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
 import com.battlelancer.seriesguide.api.Intents;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,25 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.buttonMainViewShow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // view show in SeriesGuide
-                Intent intent = Intents.buildViewShowIntent(248835);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
+        findViewById(R.id.buttonMainViewShow).setOnClickListener(v -> {
+            // view show in SeriesGuide
+            Intent intent = Intents.buildViewShowIntent(248835);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
             }
         });
 
-        findViewById(R.id.buttonMainViewEpisode).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // view episode in SeriesGuide
-                Intent intent = Intents.buildViewEpisodeIntent(248835, 4164086);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
+        findViewById(R.id.buttonMainViewEpisode).setOnClickListener(v -> {
+            // view episode in SeriesGuide
+            Intent intent = Intents.buildViewEpisodeIntent(248835, 4164086);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
             }
         });
     }
