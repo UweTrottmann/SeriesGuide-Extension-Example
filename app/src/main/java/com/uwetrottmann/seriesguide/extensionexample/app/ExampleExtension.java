7518826367472089ba187ae332ca59aa9186042f
Uwe Trottmann <uwe+git@uwetrottmann.com>
2020-07-09 08:49:19 +0200

    ExampleExtension: avoid deprecated API warning by not using imports for docs.

--- app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/ExampleExtension.java
+++ app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/ExampleExtension.java
@@ -1,20 +1,16 @@
 package com.uwetrottmann.seriesguide.extensionexample.app;
 
-import android.app.IntentService;
-
 import com.battlelancer.seriesguide.api.SeriesGuideExtension;
-
-import androidx.core.app.JobIntentService;
 
 /**
  * Optional: Original {@link SeriesGuideExtension} subclass, now subclass of
  * {@link ExampleExtensionService} for backwards compatibility with API v1.
  * <p>
  * Technically we could re-use the existing {@link SeriesGuideExtension} subclass. But as it is now
- * a subclass of {@link JobIntentService} (previously {@link IntentService}) it requires the
- * BIND_JOB_SERVICE permission on Android O+. This would prevent SeriesGuide from starting the
- * service. So we register {@link ExampleExtensionService} separately and keep the original service
- * component for this class in the manifest.
+ * a subclass of {@link androidx.core.app.JobIntentService} (previously {@link android.app.IntentService})
+ * it requires the BIND_JOB_SERVICE permission on Android O+.
+ * This would prevent SeriesGuide from starting the service. So we register {@link ExampleExtensionService}
+ * separately and keep the original service component for this class in the manifest.
  */
 public class ExampleExtension extends ExampleExtensionService {
 }
