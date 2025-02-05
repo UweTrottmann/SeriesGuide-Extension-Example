f95af8e33d0881e5ed2a7ddcad582a265b5459be
kerpi4 <kerpi4a@gmail.com>
2025-02-05 19:54:42 +0200

    Update ExampleExtensionService.kt

--- app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/ExampleExtensionService.kt
+++ app/src/main/java/com/uwetrottmann/seriesguide/extensionexample/app/ExampleExtensionService.kt
@@ -10,12 +10,16 @@ import com.battlelancer.seriesguide.api.SeriesGuideExtension
 
 open class ExampleExtensionService : SeriesGuideExtension(NAME) {
 
-    override fun onRequest(episodeIdentifier: Int, episode: Episode) {
-        if (BuildConfig.DEBUG) {
-            Log.d(NAME, "onRequest: episode ${episode.toBundle()}")
-        }
-
-        publishGoogleAction(episodeIdentifier, "tv?tmdb=${episode.showTmdbId}&season=${episode.season}&episode=${episode.number}")
+    private fun getFormattedSearchTerm(template: String, movie: Movie? = null, episode: Episode? = null): String {
+    return template
+        .replace("{movie.title}", movie?.title ?: "")
+        .replace("{movie.tmdbId}", movie?.tmdbId?.toString() ?: "")
+        .replace("{episode.title}", episode?.title ?: "")
+        .replace("{episode.showTmdbId}", episode?.showTmdbId?.toString() ?: "")
+        .replace("{episode.number}", episode?.number?.toString() ?: "")
+        .replace("{episode.season}", episode?.season?.toString() ?: "")
+        .replace("{episode.showTitle}", episode?.showTitle ?: "")
+        .replace("{episode.absoluteNumber}", episode?.absoluteNumber ?: "")
     }
 
     override fun onRequest(movieIdentifier: Int, movie: Movie) {
@@ -23,15 +27,31 @@ open class ExampleExtensionService : SeriesGuideExtension(NAME) {
             Log.d(NAME, "onRequest: movie ${movie.toBundle()}")
         }
 
-        publishGoogleAction(movieIdentifier, "movie?tmdb=${movie.tmdbId}")
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val template = sharedPreferences.getString("saved_text", "https://google.com/search?q={movie.title}") ?: "default search"
+        val searchTerm = getFormattedSearchTerm(template, movie = movie)
+
+        publishGoogleAction(movieIdentifier, searchTerm)
+    }
+
+    override fun onRequest(episodeIdentifier: Int, episode: Episode) {
+        if (BuildConfig.DEBUG) {
+            Log.d(NAME, "onRequest: episode ${episode.toBundle()}")
+        }
+    
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val template = sharedPreferences.getString("saved_text", "https://google.com/search?q={episode.showTitle) S{episode.season}E{episode.absoluteNumber}") ?: "default search"
+        val searchTerm = getFormattedSearchTerm(template, episode = episode)
+    
+        publishGoogleAction(episodeIdentifier, searchTerm)
     }
 
     private fun publishGoogleAction(identifier: Int, searchTerm: String) {
         publishAction(
-            Action.Builder("VidSrc", identifier)
+            Action.Builder("custom search", identifier)
                 .viewIntent(
                     Intent(Intent.ACTION_VIEW)
-                        .setData(Uri.parse("https://vidsrc.xyz/embed/$searchTerm&ds_lang=bg"))
+                        .setData(Uri.parse(searchTerm))
                 )
                 .build()
         )
